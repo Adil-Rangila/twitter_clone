34ab4f8df6120abeb61fe1674ee57243d41d38d4
Adil Ahmed <adil.rangila@gmail.com>
2020-09-04 00:24:20 -0700

    facting tweet with all type is completed and tested

--- lib/pages/tweets.dart
+++ lib/pages/tweets.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/add_tweet.dart';
 import 'package:twitter_clone/util/variables.dart';
@@ -31,55 +32,91 @@ class _TweetsPageState extends State<TweetsPage> {
           Icon(Icons.star, size: 32),
         ],
       ),
-      body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(urlImage),
-                ),
-                title: Text(
-                  'Adil Ahmed',
-                  style: myStyle(20, Colors.black, FontWeight.w600),
-                ),
-                subtitle: Column(
-                  children: [
-                    Text(
-                      'Looks like its working Profperly',
-                      style: myStyle(20, Colors.black, FontWeight.w400),
+      body: StreamBuilder(
+          //stream builder provide active dataflow and changes the ui base on database update
+          stream: tweetcollection.snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return CircularProgressIndicator();
+            }
+            return ListView.builder(
+                //item count is to show the number of tweet
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (BuildContext context, int index) {
+                  //in this part i am receinved tweet data as doucment so i can show in my app
+                  DocumentSnapshot tD = snapshot.data.documents[index];
+                  // print(tD.data());
+                  return Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            NetworkImage(tD.data()['profilephoto']),
+                      ),
+                      title: Text(
+                        tD.data()['name'],
+                        style: myStyle(20, Colors.black, FontWeight.w500),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //first type .....................
+                          if (tD.data()['type'] == 1)
+                            Text(
+                              tD.data()['tweet'],
+                              style: myStyle(20, Colors.black, FontWeight.w300),
+                            ),
+                          //second type..........
+                          if (tD.data()['type'] == 2)
+                            Image.network(
+                              tD.data()['image'],
+                            ),
+                          //third type .................
+                          if (tD.data()['type'] == 3)
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  tD.data()['tweet'],
+                                  style: myStyle(
+                                      20, Colors.black, FontWeight.w300),
+                                ),
+                                Image.network(
+                                  tD.data()['image'],
+                                ),
+                              ],
+                            ),
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(Icons.favorite_border),
+                                  SizedBox(width: 10),
+                                  Text(tD.data()['likes'].length.toString()),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.message),
+                                  SizedBox(width: 10),
+                                  Text(tD.data()['commentcount'].toString()),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(Icons.share),
+                                  SizedBox(width: 10),
+                                  Text(tD.data()['shares'].toString()),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.message),
-                            SizedBox(width: 10),
-                            Text('4'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.favorite_border),
-                            SizedBox(width: 10),
-                            Text('4'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.share),
-                            SizedBox(width: 10),
-                            Text('4'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
+                  );
+                });
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
--- lib/util/variables.dart
+++ lib/util/variables.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,3 +13,10 @@ myStyle(double size, [Color color, FontWeight fw]) {
 
 var urlImage =
     'https://cdn.imgbin.com/2/4/15/imgbin-computer-icons-portable-network-graphics-avatar-icon-design-avatar-DsZ54Du30hTrKfxBG5PbwvzgE.jpg';
+
+CollectionReference usercollection =
+    FirebaseFirestore.instance.collection('users');
+
+CollectionReference tweetcollection =
+    FirebaseFirestore.instance.collection('tweets');
+StorageReference pictures = FirebaseStorage.instance.ref().child('tweetpics');
