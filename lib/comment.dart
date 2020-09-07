33f01798a26d4b93685127a43e0a4c1c69e5b562
Adil Ahmed <adil.rangila@gmail.com>
2020-09-07 05:01:40 -0700

    Comment Section is completed

--- lib/comment.dart
+++ lib/comment.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/util/variables.dart';
+import 'package:timeago/timeago.dart' as tAgo;
 
 class CommentPage extends StatefulWidget {
   final String documentId;
@@ -25,33 +26,80 @@ class _CommentPageState extends State<CommentPage> {
       'profilepic': userData.data()['userphoto'],
       'time': DateTime.now()
     });
-    //print(userData.data()['username']);
+
+    DocumentSnapshot tweetCount =
+        await tweetcollection.doc(widget.documentId).get();
+
+    tweetcollection
+        .doc(widget.documentId)
+        .update({'commentcount': tweetCount.data()['commentcount'] + 1});
+
+    _commentController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Comments',
-          style: myStyle(20),
-        ),
-      ),
-      body: ListTile(
-        title: TextFormField(
-          controller: _commentController,
-          decoration: InputDecoration(
-            hintText: 'Add Comment...',
-            hintStyle: myStyle(18),
-          ),
-        ),
-        trailing: OutlineButton(
-          borderSide: BorderSide.none,
-          onPressed: () => addComment(),
-          child: Text(
-            'Publish',
-            style: myStyle(18),
+      body: SingleChildScrollView(
+        physics: ScrollPhysics(),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder(
+                    stream: tweetcollection
+                        .doc(widget.documentId)
+                        .collection('comments')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      print(snapshot);
+                      return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot userData =
+                                snapshot.data.documents[index];
+                            return ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                backgroundImage:
+                                    NetworkImage(userData.data()['profilepic']),
+                              ),
+                              title: Text(
+                                userData.data()['username'],
+                                style: myStyle(20),
+                              ),
+                              subtitle: Text(
+                                userData.data()['comment'],
+                                style: myStyle(16),
+                              ),
+                              trailing: Text(
+                                tAgo.format(userData.data()['time'].toDate()),
+                              ),
+                            );
+                          });
+                    }),
+              ),
+              ListTile(
+                title: TextFormField(
+                  controller: _commentController,
+                  decoration: InputDecoration(
+                    hintText: 'Add Comment...',
+                    hintStyle: myStyle(18),
+                  ),
+                ),
+                trailing: OutlineButton(
+                  borderSide: BorderSide.none,
+                  onPressed: () => addComment(),
+                  child: Text(
+                    'Publish',
+                    style: myStyle(18),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
