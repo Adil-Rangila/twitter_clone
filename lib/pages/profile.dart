7d72a3f3065ebe6caf8965ee1377529ff7cbd3ea
Adil Ahmed <adil.rangila@gmail.com>
2020-09-07 23:37:29 -0700

    Profile page design 50% completed

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -9,10 +9,70 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        'Profile',
-        style: myStyle(30),
+    return SingleChildScrollView(
+      child: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 4,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.lightBlue, Colors.purple],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width / 2 - 64,
+              top: MediaQuery.of(context).size.height / 6,
+            ),
+            child: CircleAvatar(
+              radius: 64,
+              backgroundImage: NetworkImage(urlImage),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height / 2.7),
+            child: Column(
+              children: [
+                Text(
+                  'Adil',
+                  style: myStyle(30, Colors.black, FontWeight.w600),
+                ),
+                SizedBox(height: 7),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Followers',
+                      style: myStyle(20, Colors.black, FontWeight.w500),
+                    ),
+                    Text(
+                      'Following',
+                      style: myStyle(20, Colors.black, FontWeight.w500),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 7),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      '500K',
+                      style: myStyle(20, Colors.black, FontWeight.w500),
+                    ),
+                    Text(
+                      '10',
+                      style: myStyle(20, Colors.black, FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
